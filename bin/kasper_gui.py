5a768dcb03ea90fc3be113ff73f6ee603fcda5f2
Abdellah El Morabit <nsrddyn@gmail.com>
2024-11-08 13:45:22 +0100

    testing a comment

--- bin/kasper_gui.py
+++ bin/kasper_gui.py
@@ -3,8 +3,8 @@ from tkinter import PhotoImage
 
 display = tkinter.Tk()
 display.title("Kasper")
-photo = PhotoImage(file = "icon.png")
+photo = PhotoImage(file="icon.png")
 display.iconphoto(False, photo)
 
-
+# making 2 versions IOS AND DESKTOP
 display.mainloop()
