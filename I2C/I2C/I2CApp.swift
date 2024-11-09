bd4adb5206b2ebedea97cd527f98db14ccabd1a4
Abdellah El Morabit <nsrddyn@gmail.com>
2024-11-09 21:57:50 +0100

    uplaoding the ios version

---
+++ I2C/I2C/I2CApp.swift
@@ -0,0 +1,17 @@
+//
+//  I2CApp.swift
+//  I2C
+//
+//  Created by nsrddyn on 09/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct I2CApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ I2C/I2CTests/I2CTests.swift
@@ -0,0 +1,17 @@
+//
+//  I2CTests.swift
+//  I2CTests
+//
+//  Created by nsrddyn on 09/11/2024.
+//
+
+import Testing
+@testable import I2C
+
+struct I2CTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
