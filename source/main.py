35b476a90b585186161ab91ef9b73d029a2ee834
Abdellah El Morabit <nsrddyn@gmail.com>
2024-11-29 23:54:15 +0100

    made the first form of a practicle gui

---
+++ source/main.py
@@ -0,0 +1,41 @@
+from PySide6.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
+from PySide6.QtCore import Qt
+from hardware_driver import lcd
+import sys
+
+
+L = lcd()
+
+class mainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("I2C CONTROLLER")
+        self.setGeometry(200,100,800,300)
+        self.mainUI()
+
+    def mainUI(self):
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_Widget)
+
+        self.label = QLabel("CONNECTED TO I2C DEVICE", self)
+        self.label.setAlignment(Qt.AlignCenter)
+
+        self.button = QPushButton("Clear Screen", self)
+        self.button.clicked.connect(self.clear_screen)
+        
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.button)
+
+        central_widget.setLayout(layout)
+
+    def clear_screen():
+        lcd.clear()
+        self.label.setText("Cleared the LCD screen", self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = mainWindow()
+    window.show()
+    sys.exit(app.exec())
+
